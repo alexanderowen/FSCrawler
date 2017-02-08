bae2c83decb485f412c00f0515599c9d90261ccc
Alexander Owen <owen.alex47@yahoo.com>
2017-02-07 16:32:11 -0800

    Simple file crawler: main.go

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"sync"
+	"time"
+)
 
 type Node struct {
 	val string
@@ -15,7 +22,7 @@ type Queue struct {
 
 func NewQueue(num int) *Queue {
 	return &Queue{
-		list:        make([]*Node, 10),
+		list:        make([]*Node, 0),
 		cond:        &sync.Cond{L: &sync.Mutex{}},
 		numRoutines: num,
 		numWaiting:  0,
@@ -50,11 +57,65 @@ func (q *Queue) Len() int {
 	return len(q.list)
 }
 
+func crawler(workQ *Queue, reg *regexp.Regexp) {
+	for {
+		n := workQ.Pop()
+		if n == nil {
+			return //all done
+		}
+
+		fmt.Printf("Testing: '%s'\n", n.val)
+		files, err := ioutil.ReadDir(n.val)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Found an error: '%s'\n", err)
+			continue
+		}
+		for _, file := range files {
+			if file.IsDir() {
+				workQ.Push(&Node{val: n.val + "/" + file.Name()})
+			}
+			if reg.MatchString(file.Name()) {
+				fmt.Printf("Found a match: %s\n", file.Name())
+			}
+		}
+	}
+}
+
+func convertToRegexp(pat string) string {
+	var reg string
+	for _, char := range pat {
+		switch char {
+		case '*':
+			reg = reg + ".*"
+		case '.':
+			reg = reg + "\\."
+		case '?':
+			reg = reg + "."
+		default:
+			reg = reg + string(char)
+		}
+	}
+	return reg
+}
+
 func main() {
+	pattern := os.Args[1]
+	var validID = regexp.MustCompile(convertToRegexp(pattern))
+
 	work := NewQueue(1)
 	n := &Node{
-		val: "hello",
+		val: ".",
 	}
 	work.Push(n)
-	work.Pop()
+	go crawler(work, validID)
+	/*
+		work.cond.L.Lock()
+		for work.numWaiting != work.numRoutines {
+			work.cond.Wait()
+		}
+		work.cond.L.Unlock()
+	*/
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("Main finished.")
+
 }
